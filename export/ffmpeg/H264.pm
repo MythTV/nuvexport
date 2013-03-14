623e3e7f2138665f00b341c0dfcfa761495f51ef
Chris Petersen <cpetersen@mythtv.org>
2013-03-13 22:35:49 -0700

    Tone down -refs to more-sane values

--- export/ffmpeg/H264.pm
+++ export/ffmpeg/H264.pm
@@ -254,7 +254,7 @@ package export::ffmpeg::H264;
             }
         }
     # Single/final pass options
-        $ffmpeg_xtra .= ' -refs '.($self->val('ipod') ? 2 : ($self->val('x264_tune') eq 'animation' ? 16 : 8))
+        $ffmpeg_xtra .= ' -refs '.($self->val('ipod') ? 2 : ($self->val('x264_tune') eq 'animation' ? 12 : 6))
                        .' -subq 7'
                        .' -partitions parti4x4+parti8x8+partp4x4+partp8x8+partb8x8'
                        .' -me_range 21'
